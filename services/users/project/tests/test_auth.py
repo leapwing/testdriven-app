148af8fc898204e379171054909081b01246bc7f
leapwing <leapwing@gmail.com>
2018-09-25 10:55:50 +0800

    remove unused models

--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -3,8 +3,6 @@ import unittest
 
 from flask import current_app
 
-from project import db
-from project.api.models import User
 from project.tests.base import BaseTestCase
 from project.tests.utils import add_user
 
