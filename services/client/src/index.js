b947dff1d4c128f07136ac9c07d1b9c51e7be136
leapwing <leapwing@gmail.com>
2018-09-25 18:02:30 +0800

    add client router

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,73 +1,13 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-// import registerServiceWorker from "./registerServiceWorker";
-import axios from "axios";
-import UsersList from "./components/UsersList";
-import AddUser from "./components/AddUser";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import registerServiceWorker from './registerServiceWorker';
+import App from './App';
 
-class App extends Component {
-  // eslint-disable-next-line
-  constructor() {
-    super();
-    this.state = { users: [], username: "", email: "" };
-    this.addUser = this.addUser.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  componentDidMount() {
-    this.getUsers();
-  }
-
-  getUsers() {
-    axios
-      .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-      .then(res => {
-        this.setState({ users: res.data.data.users });
-        console.log(this.state.users);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  addUser(event) {
-    event.preventDefault();
-    console.log("sanity check!");
-    console.log(this.state);
-  }
-
-  handleChange(event) {
-    const obj = {};
-    obj[event.target.name] = event.target.value;
-    this.setState(obj);
-  }
-
-  render() {
-    return (
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-half">
-              <br />
-              <h1 className="title is-1">All Users</h1>
-              <hr />
-              <br />
-              <AddUser
-                username={this.state.username}
-                email={this.state.email}
-                addUser={this.addUser}
-                handleChange={this.handleChange}
-              />
-              <br />
-              <br />
-              <UsersList users={this.state.users} />
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('root')
+);
 // registerServiceWorker();
