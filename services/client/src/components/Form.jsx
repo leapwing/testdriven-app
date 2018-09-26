4ce3ba29e8159c70fc47fee248b390695f337c66
leapwing <leapwing@gmail.com>
2018-09-26 11:43:17 +0800

    add client navbar

---
+++ services/client/src/components/Form.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Form = (props) => {
+	return (
+		<div>
+			<h1 className="title is-1">{props.formType}</h1>
+			<hr />
+			<br />
+			<form onSubmit={(event) => props.handleUserFormSubmit(event)}>
+				{props.formType === 'Register' && (
+					<div className="field">
+						<input
+							name="username"
+							type="text"
+							className="input is-medium"
+							placeholder="Enter a username"
+							required
+							value={props.formData.username}
+							onChange={props.handleFormChange}
+						/>
+					</div>
+				)}
+				<div className="field">
+					<input
+						name="email"
+						type="email"
+						className="input is-medium"
+						placeholder="Enter an email address"
+						required
+						value={props.formData.email}
+						onChange={props.handleFormChange}
+					/>
+				</div>
+				<div className="field">
+					<input
+						name="password"
+						type="password"
+						className="input is-medium"
+						placeholder="Enter a password"
+						required
+						value={props.formData.password}
+						onChange={props.handleFormChange}
+					/>
+				</div>
+				<input type="submit" className="button is-primary is-medium is-fullwidth" value="submit" />
+			</form>
+		</div>
+	);
+};
+
+export default Form;
--- services/client/src/components/__tests__/App.test.jsx
+++ services/client/src/components/__tests__/App.test.jsx
@@ -4,5 +4,6 @@ import { shallow } from 'enzyme';
 import App from '../../App';
 
 test('App renders without crashing', () => {
+	console.log = jest.fn();
 	const wrapper = shallow(<App />);
 });
