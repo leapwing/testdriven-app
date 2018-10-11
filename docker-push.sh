dac201660b901f4254a085ba51f2cbbf005c3fd2
leapwing <leapwing@gmail.com>
2018-10-11 18:52:16 +0800

    update dcoker-push

--- docker-push.sh
+++ docker-push.sh
@@ -4,21 +4,23 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || ["$TRAVIS_PULL_REQUEST" == "false" ] then
     
     if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
         export DOCKER_ENV=stage
-    elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
+        elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
         export DOCKER_ENV=prod
     fi
-
-    if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ] then
+    
+    if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ]
+    then
         curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-        unzip awscli-bundle.zip 
+        unzip awscli-bundle.zip
         ./awscli-bundle/install -b ~/bin/aws
         export PATH=~/bin:$PATH
         eval $(aws ecr get-login --region ap-southeast-1 --no-include-email)
         export TAG=$TRAVIS_BRANCH
         export REPO=$AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-1.amazonaws.com
     fi
-
-    if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ] then
+    
+    if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ]
+    then
         # users
         docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
         docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
