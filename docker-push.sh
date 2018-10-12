fd2935b4dfa230015c68444c89fd804a091880c2
leapwing <leapwing@gmail.com>
2018-10-12 09:06:45 +0800

    update taavis branch

--- docker-push.sh
+++ docker-push.sh
@@ -23,7 +23,7 @@ then
     if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ]
     then
         # users
-        docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
+        docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
         docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
         docker push $REPO/$USERS:$TAG
         # users db
@@ -31,11 +31,11 @@ then
         docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
         docker push $REPO/$USERS_DB:$TAG
         # client
-        docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=TBD
+        docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=TBD
         docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
         docker push $REPO/$CLIENT:$TAG
         # swagger
-        docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod
+        docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
         docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
         docker push $REPO/$SWAGGER:$TAG
     fi
