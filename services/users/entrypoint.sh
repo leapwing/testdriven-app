d576e571d72af3230d410f00595118051bff5c2a
leapwing <leapwing@gmail.com>
2018-10-16 12:15:05 +0800

    update deploy stage

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -8,4 +8,7 @@ done
 
 echo "PostgreSQL started"
 
-python manage.py run -h 0.0.0.0
+# python manage.py run -h 0.0.0.0
+python manage.py recreate-db
+python manage.py seed-db
+gunicorn -b 0.0.0.0:5000 manage:app
