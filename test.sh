edcef06f6d0bce22e488e65a7a660a6238311bc6
leapwing <leapwing@gmail.com>
2018-10-22 09:26:33 +0800

    update test for dev

--- test.sh
+++ test.sh
@@ -28,8 +28,8 @@ client() {
 
 e2e() {
     docker-compose -f docker-compose-stage.yml up -d --build
-    docker-compose -f docker-compose-stage.yml run users python manage.py recreate-db
-    # docker-compose -f docker-compose-stage.yml run users python manage.py recreate_db
+    # docker-compose -f docker-compose-stage.yml run users python manage.py recreate-db
+    docker-compose -f docker-compose-stage.yml run users python manage.py recreate_db
     ./node_modules/.bin/cypress run --config baseUrl=http://localhost
     inspect $? e2e
     docker-compose -f docker-compose-stage.yml down
