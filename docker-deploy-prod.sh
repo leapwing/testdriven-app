ff741ed8ee0ad6050ff7cd56f5208b17a1831dfe
leapwing <leapwing@gmail.com>
2018-11-07 09:19:26 +0800

    update prod deploy

--- docker-deploy-prod.sh
+++ docker-deploy-prod.sh
@@ -6,14 +6,14 @@ then
     if [ "$TRAVIS_BRANCH" == "production" ]
     then
         JQ="jq --raw-output --exit-status"
-
+        
         configure_aws_cli() {
             aws --version
             aws configure set default.region ap-southeast-1
             aws configure set default.output json
             echo "AWS configured!"
         }
-
+        
         register_definition() {
             if revision=$(aws ecs register-task-definition --cli-input-json "$task_def" | $JQ '.taskDefinition.taskDefinitionArn'); then
                 echo "Revision: $revision"
@@ -22,17 +22,17 @@ then
                 return 1
             fi
         }
-
+        
         update_service() {
-            if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then    
+            if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
                 echo "Error updating service."
                 return 1
             fi
         }
-
+        
         deploy_cluster() {
-
-            cluster="test-driven-production-cluster" 
+            
+            cluster="test-driven-production-cluster"
             # users
             service="testdriven-users-prod-service"
             template="ecs_users_prod_taskdefinition.json"
@@ -41,7 +41,7 @@ then
             echo "$task_def"
             register_definition
             update_service
-
+            
             # client
             service="testdriven-client-prod-service"
             template="ecs_client_prod_taskdefinition.json"
@@ -50,7 +50,7 @@ then
             echo "$task_def"
             register_definition
             update_service
-
+            
             # swagger
             service="testdriven-swagger-prod-service"
             template="ecs_swagger_prod_taskdefinition.json"
@@ -59,7 +59,7 @@ then
             echo "$task_def"
             register_definition
             update_service
-
+            
             # exercise
             service="testdriven-exercises-prod-service"
             template="ecs_exercises_prod_taskdefinition.json"
@@ -68,10 +68,19 @@ then
             echo "$task_def"
             register_definition
             update_service
+            
+            # scores
+            service="testdriven-scores-prod-service"
+            template="ecs_scores_prod_taskdefinition.json"
+            task_template=$(cat "ecs/$template")
+            task_def=$(printf "$task_template" $AWS_RDS_SCORES_URI $AWS_ACCOUNT_ID)
+            echo "$task_def"
+            register_definition
+            # update_service
         }
         
         configure_aws_cli
         deploy_cluster
-
+        
     fi
 fi
